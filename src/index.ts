19cdee8a8d450786e297e888f87cbacc04551d5c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-09-04 23:44:58 +0200

    chore: setup repo

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { faker } from "faker-js/faker";
+
+const firstName: string = faker.person.firstName();
+const lastName: string = faker.person.lastName();
+
+console.log(firstName);
+console.log(lastName);
